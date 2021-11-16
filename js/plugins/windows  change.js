9002134f6ecc0b9020c6b7bd9e3dc99568762e03
aquced <aqucedwgk@gmail.com>
2021-11-16 14:38:57 +0800

    001

---
+++ js/plugins/windows  change.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+SceneManager._screenWidth       = 1296;
+SceneManager._screenHeight      = 720;
+SceneManager._boxWidth          = 816;
+SceneManager._boxHeight         = 624;
